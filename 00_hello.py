a24058687da78f1bec741a98586f101afc8d4dd1
skylit <cesaralonso227@gmail.com>
2022-11-19 21:49:02 -0500

    cambio al fichero

--- 00_hello.py
+++ 00_hello.py
@@ -11,5 +11,9 @@ Este es un comentario en varias lineas, igual puede ser con comas simples
 """
 
 #consultar el tipo de dato
-print (type("hola mundo")) #tipo string
+print (type("dato str")) #tipo string
+print (type(5)) #tipo int
+print (type(1.5)) #tipo int
+print (type(True)) #tipo bool
+print (type(1+2j)) #tipo complex
 
