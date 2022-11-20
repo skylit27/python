62bf53bbd64fafc11d6a03f9711d476e4426da5a
skylit <cesaralonso227@gmail.com>
2022-11-19 21:42:55 -0500

    mi primer archivo

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Esto es un comentario
+#Nuestro hola mundo en python
+print ("hola python")
+print ('hola python')
+
+
+
+"""
+Este es un comentario en varias lineas, igual puede ser con comas simples
+
+"""
+
+#consultar el tipo de dato
+print (type("hola mundo")) #tipo string
+
