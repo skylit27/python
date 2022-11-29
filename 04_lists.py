aa20439afb4976a61e5c506c49950c1e2910b041
skylit <cesaralonso227@gmail.com>
2022-11-28 22:46:04 -0500

    nuevos cambios a lists

--- 04_lists.py
+++ 04_lists.py
@@ -9,3 +9,11 @@ my_list = [35, 53, 58, 30, 30]
 
 print(my_list)
 print(len(my_list))
+
+my_other_list = [35 , 1.74, "drackoss", "skylit"]
+
+print(type(my_list))
+print(type(my_other_list))
+print(type(my_other_list[0]))
+print(my_other_list[2])
+
