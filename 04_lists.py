4511889bb81373988a54bddd6feee475dff90440
skylit <cesaralonso227@gmail.com>
2022-11-29 22:19:21 -0500

    nuevos cambios a lista

--- 04_lists.py
+++ 04_lists.py
@@ -25,3 +25,42 @@ print(my_other_list[-1])
 
 print(my_other_list.count("drackoss")) #count sirve para contar cuantas veces hay un dato
 print(my_list.count(30)) #count sirve para contar cuantas veces hay un dato
+
+age, height, name, surname = my_other_list #agarra el dato dependiendo de la posicion en la lista
+
+print(name) #name es el tercer dato de my_other_list
+print(age) 
+
+age, height, name, surname = my_other_list[2], my_other_list[1], my_other_list[0], my_other_list[3], #cambiamos las posiciones
+print(age) 
+
+print(my_list + my_other_list) # suma de listas
+
+my_other_list.append("skylit27") #con append agrego un nuevo dato a la lista, esta se va al final
+print(my_other_list)
+
+my_other_list.insert(1, "negro") 
+
+"""insert: inserta en una posicion de la lista 
+un nuevo dato desplazando el dato que esta en 
+esta posicion a la derecha en este caso el dato de la posicion 1 pasa
+a la posicion 2
+"""
+print(my_other_list)
+
+
+my_list= "Hola Python" #convertimos la lista anterior a string # Es un tipado dinamico de python
+
+print(my_list)
+print(type(my_list))
+
+my_list= ["Hola Python"] #aqui la volvemos a convertir en lista
+
+print(my_list)
+print(type(my_list))
+
+#no se pueden crear constantes en python como tal, se puede definir en mayuscula para indicar que es una constante
+
+
+
+
