ae721a12e07ef4e555ffeb159b54f429ee34b75a
skylit <cesaralonso227@gmail.com>
2022-11-29 21:20:35 -0500

    cambios a lista

--- 04_lists.py
+++ 04_lists.py
@@ -15,5 +15,13 @@ my_other_list = [35 , 1.74, "drackoss", "skylit"]
 print(type(my_list))
 print(type(my_other_list))
 print(type(my_other_list[0]))
-print(my_other_list[2])
 
+
+print(my_other_list[2])
+print(my_other_list[-1])
+
+#print(my_other_list[-5]) #error
+#print(my_other_list[4]) #error
+
+print(my_other_list.count("drackoss")) #count sirve para contar cuantas veces hay un dato
+print(my_list.count(30)) #count sirve para contar cuantas veces hay un dato
