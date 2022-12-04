9461d7eee777ede359b7cd09b5c99d62d12a086e
skylit <cesaralonso227@gmail.com>
2022-12-04 13:55:51 -0500

    loops

--- 09_loops.py
+++ 09_loops.py
@@ -1,2 +1,15 @@
 ## loops ##
 
+# while
+
+my_condition  = 0
+
+while my_condition < 10:  #al while le podemos meter un else
+    #my_condition +=1 si lo pongo aqui empieza en 1 pero si lo dejo como abajo empieza mostrando 1
+    print(my_condition)
+    my_condition +=1
+else: print("Mi condicion es mayor o igual que 10")
+
+# si pusieramos un if  antes del else seria un if normal ya que no entraria como parte del while
+print("la ejecucion continua")
+    
